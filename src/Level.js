57e20759df5ca575636ac219d696c902406b818f
Filip Chalupa <chalupa.filip@gmail.com>
2021-01-30 00:23:08 +0100

    Base

---
+++ src/Level.js
@@ -0,0 +1,56 @@
+import WebSocket from 'ws'
+
+export class Level {
+	socketServer = new WebSocket.Server({ noServer: true })
+	lastIdAssigned = 0
+	clients = {}
+
+	constructor(id) {
+		this.id = id
+
+		this.socketServer.on('connection', this.onConnection)
+		this.socketServer.on('close', this.onClose)
+	}
+
+	handleIncomingConnection = (request, socket, head) => {
+		this.socketServer.handleUpgrade(request, socket, head, (ws) => {
+			this.socketServer.emit('connection', ws)
+		})
+	}
+
+	onConnection = (socket) => {
+		console.log('on connection')
+		const id = `${++this.lastIdAssigned}`
+		this.clients[id] = socket
+
+		socket.on('message', (message) => {
+			console.log('new message',message.toString())
+			const data = JSON.parse(message.toString())
+			if ('position' in data) {
+				for (const [otherId, otherSocket] of Object.entries(this.clients)) {
+					if (id !== otherId) {
+						otherSocket.send(JSON.stringify({
+							id,
+							position: data.position,
+						}))
+					}
+				}
+			}
+			this.socketServer
+		})
+
+		socket.on('close', () => {+
+			delete this.clients[id]
+			for (const [otherId, otherSocket] of Object.entries(this.clients)) {
+				otherSocket.send(JSON.stringify({
+					id,
+					left: true,
+				}))
+			}
+		})
+	}
+
+	onClose = (socket) => {
+		console.log('on close')
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import http from 'http'
+import url from 'url'
+import { Level } from './Level.js'
+
+console.log('Starting server')
+
+const port = parseInt(process.env.PORT || '', 10) || 8080
+
+const app = express()
+
+const server = http.createServer(app)
+
+const levels = []
+
+server.on('upgrade', (request, socket, head) => {
+	const { pathname } = url.parse(request.url)
+
+	const match = pathname?.match(/\/level\/(.*)\.ws/)
+	const levelId = match ? match[1] : null
+
+	if (levelId) {
+		// @TODO: release unused level from memory
+		const level =
+			levels.find((level) => levelId === level.id) ||
+			(() => {
+				const newLevel = new Level(levelId)
+				levels.push(newLevel)
+				return newLevel
+			})()
+
+		level.handleIncomingConnection(request, socket, head)
+	} else {
+		socket.destroy()
+	}
+})
+
+server.listen(port, () => {
+	console.log(`Server started on port ${port}`)
+})
